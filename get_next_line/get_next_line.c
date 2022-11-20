87262dcacf76bec7a520ed92b103fc242e0cce89
mk3058 <kmk3058@icloud.com>
2022-11-20 11:42:26 +0900

    mandatory 완성

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minkyu <minkyu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 21:22:52 by minkyu            #+#    #+#             */
-/*   Updated: 2022/11/19 23:55:33 by minkyu           ###   ########.fr       */
+/*   Updated: 2022/11/20 11:41:32 by minkyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*get_line(char *content, int read_size);
+static char	*get_line(char *content, int rd_size);
 static void	*ft_memcpy(void *dst, const void *src, size_t n);
-static void	ft_lstdelone(t_list *file_list, t_list *lst);
+static void	ft_lstdelone(t_list **file_list, t_list *lst);
 
 char	*get_next_line(int fd)
 {
@@ -22,27 +22,27 @@ char	*get_next_line(int fd)
 	t_list			*cur_file;
 	char			buf[BUFFER_SIZE + 1];
 	char			*line;
-	int				read_size;
+	int				rd_size;
 
 	cur_file = get_list(&file_list, fd);
-	read_size = BUFFER_SIZE;
+	rd_size = BUFFER_SIZE;
 	line = cur_file -> content;
 	while (line == cur_file -> content)
 	{
 		ft_bzero(buf, BUFFER_SIZE + 1);
-		read_size = read(fd, buf, BUFFER_SIZE);
-		if (read_size <= 0 && ! ft_strlen(cur_file -> content))
+		rd_size = read(fd, buf, BUFFER_SIZE);
+		if ((rd_size == 0 && !ft_strlen(cur_file -> content)) || rd_size < 0)
 		{
-			ft_lstdelone(file_list, cur_file);
-			return (0);
+			ft_lstdelone(&file_list, cur_file);
+			return (NULL);
 		}
 		cur_file -> content = ft_strjoin(cur_file -> content, buf);
-		line = get_line(cur_file -> content, read_size);
+		line = get_line(cur_file -> content, rd_size);
 	}
 	return (line);
 }
 
-static char	*get_line(char *content, int read_size)
+static char	*get_line(char *content, int rd_size)
 {
 	int		i;
 	int		line_size;
@@ -58,13 +58,13 @@ static char	*get_line(char *content, int read_size)
 			break ;
 		}
 	}
-	if (!line_size && read_size < BUFFER_SIZE)
+	if (!line_size && rd_size < BUFFER_SIZE)
 		line_size = i;
-	else if (!line_size && read_size == BUFFER_SIZE)
+	else if (!line_size && rd_size == BUFFER_SIZE)
 		return (content);
 	line = malloc(sizeof(char) * line_size + 1);
 	if (line == 0)
-		return (0);
+		return (NULL);
 	ft_bzero(line, line_size + 1);
 	ft_strlcat(line, content, line_size + 1);
 	ft_memcpy(content, content + line_size, ft_strlen(content + line_size) + 1);
@@ -86,20 +86,22 @@ static void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
-static void	ft_lstdelone(t_list *file_list, t_list *lst)
+static void	ft_lstdelone(t_list **file_list, t_list *lst)
 {
 	t_list	*pre;
 
-	if (!lst || !file_list)
+	if (!lst || !(*file_list))
 		return ;
 	pre = 0;
-	while (file_list -> fd != lst -> fd)
+	while ((*file_list)-> fd != lst -> fd)
 	{
-		pre = file_list;
-		file_list = file_list -> next;
+		pre = *file_list;
+		*file_list = (*file_list)-> next;
 	}
 	if (pre)
 		pre -> next = lst -> next;
+	else
+		*file_list = 0;
 	free(lst -> content);
 	free(lst);
 }
