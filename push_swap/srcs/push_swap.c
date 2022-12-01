08706a734e38f2956d87e074b559a86cd1c191f8
mk3058 <kmk3058@icloud.com>
2022-12-01 20:27:19 +0900

    devide get_pivot func

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,13 +6,14 @@
 /*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 16:40:50 by minkyuki          #+#    #+#             */
-/*   Updated: 2022/12/01 17:58:35 by minkyuki         ###   ########.fr       */
+/*   Updated: 2022/12/01 18:22:35 by minkyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 static void	devide(t_deque *a, t_deque *b);
+static void	get_pivot(char *pivot, t_deque *a);
 
 // input 중간에 문자열 있을 경우 처리 필요 1 2 3 "4 5" 6
 int	main(int argc, char **argv)
@@ -35,8 +36,7 @@ static void	devide(t_deque *a, t_deque *b)
 
 	i = -1;
 	deq_size = deque_size(a);
-	pivot[0] = (int)((double)deq_size / 3 + 0.5);
-	pivot[1] = (int)(((double)deq_size / 3) * 2 + 0.5);
+	get_pivot(pivot, a);
 	while (++i < deq_size)
 	{
 		tmp = a -> head;
@@ -54,4 +54,13 @@ static void	devide(t_deque *a, t_deque *b)
 	i = 0;
 	while (i++ < deq_size)
 		pb(a, b);
+}
+
+static void	get_pivot(char *pivot, t_deque *a)
+{
+	int	deq_size;
+
+	deq_size = deque_size(a);
+	pivot[0] = (int)((double)deq_size / 3 + 0.5);
+	pivot[1] = (int)(((double)deq_size / 3) * 2 + 0.5);
 }
